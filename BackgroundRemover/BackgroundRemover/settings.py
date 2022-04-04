3a16123e398ef48019810730be901238c6a95802
Zampilled <89923286+Zampilled@users.noreply.github.com>
2022-04-04 19:42:07 +0100

    Fixing slug size

--- BackgroundRemover/BackgroundRemover/settings.py
+++ BackgroundRemover/BackgroundRemover/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-%(w(p$ru)id^xwmwv_mhq1cnbe*6bh0v45bmn61dur$+0az=*r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bgremov.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
